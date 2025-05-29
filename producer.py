584bedd0a876f0da525d1871db23ed09456c92ac
Viktor Terentyev <prtv27@outlook.com>
2025-05-29 22:49:12 +0300

    broker producer connect

--- producer.py
+++ producer.py
@@ -27,29 +27,34 @@ async def main():
     broker = RabbitBroker(
         f"amqp://{os.getenv('RABBITMQ_USER')}:{os.getenv('RABBITMQ_PASS')}@{os.getenv('RABBITMQ_HOST')}"
     )
+    await broker.connect()
     trading_queue = RabbitQueue("trading_signals", durable=True)
     async with BybitAsyncClient(
         api_key=os.getenv("BYBIT_RO_API_KEY"),
         api_secret=os.getenv("BYBIT_RO_API_SECRET"),
         is_demo=os.getenv("BYBIT_RO_IS_DEMO", "true") == "true",
     ) as client:
-        strategy = TrandStrategy(client=client)
-        while True:
-            for ticker in ticker_list:
-                prediction = await strategy.predict(ticker)
-                logger.info(f"Prediction for {ticker}: {prediction.action}")
-                if prediction.action == ActionEnum.BUY:
-                    price = await client.get_ticker_price(prediction.symbol)
-                    message = TradingSignal(
-                        symbol=ticker,
-                        # hardcore 100usdt
-                        amount=(Decimal(100) / price).quantize(Decimal("0.00000001")),
-                        take_profit=2,
-                        stop_loss=1,
-                        action=ActionEnum.BUY,
-                    )
+        try:
+            strategy = TrandStrategy(client=client)
+            while True:
+                for ticker in ticker_list:
+                    prediction = await strategy.predict(ticker)
+                    logger.info(f"Prediction for {ticker}: {prediction.action}")
+                    if prediction.action == ActionEnum.BUY:
+                        price = await client.get_ticker_price(prediction.symbol)
+                        message = TradingSignal(
+                            symbol=ticker,
+                            # hardcore 100usdt
+                            amount=(Decimal(100) / price).quantize(Decimal("0.00000001")),
+                            take_profit=2,
+                            stop_loss=1,
+                            action=prediction.action,
+                        )
                     await broker.publish(message.model_dump(), queue=trading_queue)
-            await asyncio.sleep(600)
+                await asyncio.sleep(600)
+        finally:
+            await broker.close()
+
 
 
 if __name__ == "__main__":
